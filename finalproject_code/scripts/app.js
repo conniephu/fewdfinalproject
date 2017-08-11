4ac888e837f9adb25a268d171aaa980584d5fe31
conniephu <connie.phu@C02NG0WLG3QR.local>
2017-08-11 16:13:48 -0500

    8-11 update

--- finalproject_code/scripts/app.js
+++ finalproject_code/scripts/app.js
@@ -1,9 +1,49 @@
 window.onload = function(event) {
 	
+
+	var slides = document.querySelectorAll('.slide');
+	var currentSlide = 0;
+	var slideInterval = setInterval(nextSlide,4000);
+
+
+
+	var slides = document.querySelectorAll('#slides .slide');
+	var currentSlide = 0;
+	var slideInterval = setInterval(nextSlide,4000);
+
+	function nextSlide() {
+	    slides[currentSlide].className = 'slide';
+	    currentSlide = (currentSlide+1)%slides.length;
+	    slides[currentSlide].className = 'slide showing';
+	}
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+
+function pauseSlideshow() {
+    pauseButton.innerHTML = 'Play';
+    playing = false;
+    clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+    pauseButton.innerHTML = 'Pause';
+    playing = true;
+    slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function() {
+    if(playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+};
+
+
 }
 
 
-  $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-        
+
+
+
